7625d2e6f0294f890214f5cab9e01dd46bffa17e
Ivan <serene.ease@gmail.com>
2024-01-16 20:29:33 +0200

    StatusClass fixed

--- src/components/Friend/Friend.jsx
+++ src/components/Friend/Friend.jsx
@@ -1,12 +1,13 @@
+import clsx from 'clsx';
 import css from './Friend.module.css';
 
 export default function Friend({ user: { name, isOnline, avatar } }) {
-  const status = isOnline ? css.online : css.offline;
+  const statusClass = isOnline ? css.isOnline : css.isOffline;
   return (
     <div className={css.card}>
       <img className={css.avatar} src={avatar} alt="User photo" />
       <p className={css.name}>{name}</p>
-      <p className={css.status}>{isOnline ? 'Online' : 'Offline'}</p>
+      <p className={statusClass}>{isOnline ? 'Online' : 'Offline'}</p>
     </div>
   );
 }
